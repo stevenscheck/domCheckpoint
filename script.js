a96c887b5319d42fa25df1a195eaa26f3f3a8360
Steven Scheck <111304697+stevenscheck@users.noreply.github.com>
2022-11-14 07:12:18 -0800

    scripts

---
+++ script.js
@@ -0,0 +1,174 @@
+/* eslint-disable no-alert */
+
+const { ALL } = require("dns")
+
+/**************
+ *   SLICE 1
+ **************/
+
+function updateCoffeeView(coffeeQty) {
+  // your code here
+  document.querySelector('#coffee_counter').innerText = coffeeQty
+}
+
+function clickCoffee(data) {
+  data.coffee++
+  updateCoffeeView(data.coffee)
+  // your code here
+}
+
+/**************
+ *   SLICE 2
+ **************/
+
+function unlockProducers(producers, coffeeCount) {
+  // your code here
+  for(const pro of producers){
+    if(coffeeCount >= (pro.price/2)){
+      pro.unlocked = true
+    }
+  }
+}
+
+function getUnlockedProducers(data) {
+  // your code here
+  return data.producers.filter((producer) => {
+    return producer.unlocked
+  })
+}
+
+function makeDisplayNameFromId(id) {
+  // your code here
+  let wow = id.split('_')
+  return wow.map((word) => {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase()
+  }).join(' ')
+}
+
+// You shouldn't need to edit this function-- its tests should pass once you've written makeDisplayNameFromId
+function makeProducerDiv(producer) {
+  const containerDiv = document.createElement('div');
+  containerDiv.className = 'producer';
+  const displayName = makeDisplayNameFromId(producer.id);
+  const currentCost = producer.price;
+  const html = `
+  <div class="producer-column">
+    <div class="producer-title">${displayName}</div>
+    <button type="button" id="buy_${producer.id}">Buy</button>
+  </div>
+  <div class="producer-column">
+    <div>Quantity: ${producer.qty}</div>
+    <div>Coffee/second: ${producer.cps}</div>
+    <div>Cost: ${currentCost} coffee</div>
+  </div>
+  `;
+  containerDiv.innerHTML = html;
+  return containerDiv;
+}
+
+function deleteAllChildNodes(parent) {
+  // your code here
+  while (parent.firstChild){
+  parent.removeChild(parent.firstChild)
+  }
+}
+
+function renderProducers(data) {
+  // your code here
+  
+  unlockProducers(data.producers, data.coffee)
+  deleteAllChildNodes(document.querySelector('#producer_container'))
+  for(const producer of data.producers)
+  if(producer.unlocked){
+  document.querySelector('#producer_container').appendChild(makeProducerDiv(producer))
+  }
+}
+
+/**************
+ *   SLICE 3
+ **************/
+
+function getProducerById(data, producerId) {
+  // your code here
+}
+
+function canAffordProducer(data, producerId) {
+  // your code here
+}
+
+function updateCPSView(cps) {
+  // your code here
+}
+
+function updatePrice(oldPrice) {
+  // your code here
+}
+
+function attemptToBuyProducer(data, producerId) {
+  // your code here
+}
+
+function buyButtonClick(event, data) {
+  // your code here
+}
+
+function tick(data) {
+  // your code here
+}
+
+/*************************
+ *  Start your engines!
+ *************************/
+
+// You don't need to edit any of the code below
+// But it is worth reading so you know what it does!
+
+// So far we've just defined some functions; we haven't actually
+// called any of them. Now it's time to get things moving.
+
+// We'll begin with a check to see if we're in a web browser; if we're just running this code in node for purposes of testing, we don't want to 'start the engines'.
+
+// How does this check work? Node gives us access to a global variable /// called `process`, but this variable is undefined in the browser. So,
+// we can see if we're in node by checking to see if `process` exists.
+if (typeof process === 'undefined') {
+  // Get starting data from the window object
+  // (This comes from data.js)
+  const data = window.data;
+
+  // Add an event listener to the giant coffee emoji
+  const bigCoffee = document.getElementById('big_coffee');
+  bigCoffee.addEventListener('click', () => clickCoffee(data));
+
+  // Add an event listener to the container that holds all of the producers
+  // Pass in the browser event and our data object to the event listener
+  const producerContainer = document.getElementById('producer_container');
+  producerContainer.addEventListener('click', event => {
+    buyButtonClick(event, data);
+  });
+
+  // Call the tick function passing in the data object once per second
+  setInterval(() => tick(data), 1000);
+}
+// Meanwhile, if we aren't in a browser and are instead in node
+// we'll need to exports the code written here so we can import and
+// Don't worry if it's not clear exactly what's going on here;
+// We just need this to run the tests in Mocha.
+else if (process) {
+  module.exports = {
+    updateCoffeeView,
+    clickCoffee,
+    unlockProducers,
+    getUnlockedProducers,
+    makeDisplayNameFromId,
+    makeProducerDiv,
+    deleteAllChildNodes,
+    renderProducers,
+    updateCPSView,
+    getProducerById,
+    canAffordProducer,
+    updatePrice,
+    attemptToBuyProducer,
+    buyButtonClick,
+    tick
+  };
+}
